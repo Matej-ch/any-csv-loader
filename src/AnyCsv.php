f08b2ec8fff693e5665f31f85daef035ef74df9e
Matej <matejchalachan@gmail.com>
2022-05-01 20:28:34 +0200

    added translation setup

--- src/AnyCsv.php
+++ src/AnyCsv.php
@@ -2,9 +2,43 @@
 
 namespace matejch\anyCsvLoader;
 
+use Yii;
 use yii\base\Module;
 
 class AnyCsv extends Module
 {
+    public $controllerNamespace = 'matejch\anyCsvLoader\controllers';
 
+    public $defaultRoute = 'page-guide/index';
+
+    public function init()
+    {
+        parent::init();
+
+        Yii::setAlias('@matejch/anyCsvLoader', __DIR__);
+
+
+        $this->registerTranslations();
+    }
+
+    public function registerTranslations()
+    {
+        if (Yii::$app->has('i18n')) {
+            Yii::$app->i18n->translations['anyCsvLoader/*'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en',
+                'forceTranslation' => true,
+                'basePath'  => '@matejch/anyCsvLoader/messages',
+                'fileMap' => [
+                    'anyCsvLoader/view' => 'view.php',
+                    'anyCsvLoader/model' => 'model.php',
+                ],
+            ];
+        }
+    }
+
+    public static function t($category, $message, $params = [], $language = null)
+    {
+        return Yii::t('anyCsvLoader/' . $category, $message, $params, $language);
+    }
 }
