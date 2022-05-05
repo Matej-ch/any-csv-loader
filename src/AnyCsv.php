bd1731662a5f091d03731344b5319d0bd4642dc9
Matej <matejchalachan@gmail.com>
2022-05-05 21:38:10 +0200

    closes #4 added attributes for file and models to module

--- src/AnyCsv.php
+++ src/AnyCsv.php
@@ -4,7 +4,9 @@ namespace matejch\anyCsvLoader;
 
 use matejch\anyCsvLoader\assets\CsvLoaderAsset;
 use Yii;
+use yii\base\InvalidConfigException;
 use yii\base\Module;
+use yii\helpers\Json;
 
 class AnyCsv extends Module
 {
@@ -12,6 +14,13 @@ class AnyCsv extends Module
 
     public $defaultRoute = 'page-guide/index';
 
+    public $models = [];
+
+    public $modelsFile = '';
+
+    /**
+     * @throws InvalidConfigException
+     */
     public function init()
     {
         parent::init();
@@ -20,10 +29,22 @@ class AnyCsv extends Module
 
         CsvLoaderAsset::register(Yii::$app->view);
 
+        if(empty($this->models) && empty($this->modelsFile)) {
+            throw new InvalidConfigException('models or modelsFile must be set');
+        }
+
+        if (empty($this->models) && !empty($this->modelsFile)) {
+            if(!($fileContents = file_get_contents($this->modelsFile))) {
+                throw new InvalidConfigException('modelsFile failed to load');
+            }
+
+            \Yii::configure($this, Json::decode($fileContents));
+        }
+
         $this->registerTranslations();
     }
 
-    public function registerTranslations()
+    public function registerTranslations(): void
     {
         if (Yii::$app->has('i18n')) {
             Yii::$app->i18n->translations['anyCsvLoader/*'] = [
